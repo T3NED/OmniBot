c434d684e1ceb583ae306fd2f3f325fa8952dd4c
Piyush <bhangalepiyush@gmail.com>
2019-03-03 12:30:30 +0530

    Bug Fixes, And some peanut Butter uh?

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   discord_token: process.env.DISCORD_TOKEN || "",
   brawl_stars_api_token: process.env.BRAWL_STARS_API || "",
-  owners: process.env.owners.split(",") || [],
+  owners: process.env.owners ? process.env.owners.split(",") : [""],
 
   //mongodb
   user: "",
