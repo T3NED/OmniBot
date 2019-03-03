b9f22220c0cbda0fb8784460985df82361122b96
Piyush <bhangalepiyush@gmail.com>
2019-03-03 11:13:09 +0530

    This tooo!

--- app.js
+++ app.js
@@ -5,5 +5,8 @@ new Client({
     prefix: ".",
     commandEditing: true,
     typing: true,
+    providers: {
+    default: "mongodb",
+  },
     readyMessage: (client) => `Successfully initialized. Ready to serve ${client.guilds.size} guilds.`
 }).login(require("./config").discord_token);
