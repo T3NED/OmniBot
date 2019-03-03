338d191427669ff4f74dffa84cfa18e151097444
Piyush <bhangalepiyush@gmail.com>
2019-03-03 11:37:32 +0530

    Beautify? Isnt It?

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const { Client } = require("klasa");
+const {Client} = require("klasa");
 
 new Client({
-    fetchAllMembers: false,
-    prefix: ".",
-    commandEditing: true,
-    typing: true,
-    providers: {
-    default: "mongodb",
+  fetchAllMembers: false,
+  prefix: ".",
+  commandEditing: true,
+  typing: true,
+  providers: {
+    default: "mongodb"
   },
-    readyMessage: (client) => `Successfully initialized. Ready to serve ${client.guilds.size} guilds.`
+  readyMessage: client =>
+    `Successfully initialized. Ready to serve ${client.guilds.size} guilds.`
 }).login(require("./config").discord_token);
