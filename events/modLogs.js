42f68e5c67b64d59bda21a0ccb87dafdfda827eb
Piyush <bhangalepiyush@gmail.com>
2019-03-08 18:42:51 +0530

    [ WIP ] - Mod Logs

---
+++ events/modLogs.js
@@ -0,0 +1,32 @@
+const {Event} = require("klasa");
+const {MessageEmbed} = require("discord.js");
+
+class modLogs extends Event {
+  constructor(...args) {
+    super(...args, {
+      enabled: true,
+      once: false
+    });
+  }
+
+  async run(guild, type, data) {
+    if (!guild.settings.loggingChannel) return;
+
+    const channel = this.client.chennels.get(
+      guild.settings.get("loggingChannel")
+    );
+
+    if (!channel || !channel.postable || !channel.embedable) return;
+
+    switch (type) {
+      case "join":
+
+      default:
+        break;
+    }
+  }
+
+  generateEmbed(message, color, data) {
+    const embed = new MessageEmbed().setColor(color).setDescription(message);
+  }
+}
