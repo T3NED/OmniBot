3b0043899aaeeb7a2d2c5cf2f7cc91b0785a387f
Phunt05 <37057540+Phunt05@users.noreply.github.com>
2019-04-04 22:33:44 +0530

    Logs now have options

--- commands/Settings/Logging/logs.js
+++ commands/Settings/Logging/logs.js
@@ -12,13 +12,13 @@ module.exports = class extends Command {
             permissionLevel: 6,
             description: 'Set the Logging Channel for the server.',
             extendedHelp: 'No extended help available.',
-            usage: '<set|disable> [channel:channelname]',
+            usage: '<channel|toggle|disable> [channel:channelname|options:str]',
             usageDelim: ' ',
-            subcommands: true,          
+            subcommands: true,
         });
     }
 
-    async set(msg, [channel]) {
+    async channel(msg, [channel]) {
         if(!channel) return msg.send(this.generateFailed(`**${msg.author}, Please provide a valid channel**`));
         await msg.guild.settings.update("log.enabled", true, msg.guild);
         await msg.guild.settings.update("log.channel", channel, msg.guild).then(() => {
@@ -33,6 +33,31 @@ module.exports = class extends Command {
         });
     }
 
+    async toggle(msg, [options]) {
+        if(!options) return msg.send(this.generateFailed('Valid Options: kick, ban, roles, message, mute'));
+        let option = options.toLowerCase();
+        switch (option) {
+            case "kick":
+                this.update("kick", msg);
+                break;
+            case "ban":
+                this.update("kick", msg);
+                break;
+            case "roles":
+                this.update("kick", msg);
+                break;
+            case "message":
+                this.update("kick", msg);
+                break;
+            case "mute":
+                this.update("kick", msg);
+                break;
+            default:
+                msg.send(this.generateFailed('Valid Options: kick, ban, roles, message, mute'));
+                break;
+        }
+    }
+
     generateSuccess(message) {
         const embed = new MessageEmbed()
             .setColor("#f48f42")
@@ -44,6 +69,15 @@ module.exports = class extends Command {
             .setColor("RED")
             .setDescription(message);
         return embed;
+    }
+    update(key, msg) {
+        if (msg.guild.settings.get(`log.${key}`)) {
+            msg.guild.settings.update(`log.${key}`, false);
+            return msg.send(this.generateSuccess(`**${msg.author}, ${key} logs are now disabled**`));
+        } else {
+            msg.guild.settings.update(`log.${key}`, true);
+            return msg.send(this.generateSuccess(`**${msg.author}, ${key} logs are now enabled**`));
+        }
     }
 
 };
