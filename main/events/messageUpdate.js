84a2526f40225352a9b20d662e29a5c89ee524f6
Phunt05 <37057540+Phunt05@users.noreply.github.com>
2019-04-14 19:32:03 +0530

    already fixed

--- main/events/messageUpdate.js
+++ main/events/messageUpdate.js
@@ -6,6 +6,7 @@ module.exports = class extends Event {
 		if (this.client.ready && !oldMsg.partial && oldMsg.content !== newMsg.content) this.client.monitors.run(newMsg);
 
 		if (!oldMsg.guild || oldMsg.author.bot || oldMsg.content === newMsg.content) return;
+		if (oldMsg.content > 1024 || newMsg.content > 1024) return;
 		this.client.emit("modLogs", oldMsg.guild, "msgUpdate", {name: "message", channel: oldMsg.channel, oldMsg: oldMsg.content, newMsg: newMsg.content}, oldMsg.author);
 	}
 
