2883e3444468739f9714115cc404948a6d430e17
Phunt05 <37057540+Phunt05@users.noreply.github.com>
2019-04-16 18:19:07 +0530

    beta tests

---
+++ main/events/guildMemberRemove.js
@@ -0,0 +1,26 @@
+const { Event } = require('klasa');
+
+module.exports = class extends Event {
+
+    async run(member) {
+
+        // Farewell Message
+        const { guild } = member;
+        if (!guild.settings.greet.leave.enabled) return;
+        const channel = guild.channels.get(guild.settings.greet.leave.channel);
+        if (!channel) return;
+        if (!channel.postable) return;
+        channel.send(this.replace(guild.settings.greet.leave.message, member));
+
+    }
+
+    replace(msg, member) {
+        return msg
+            .replace(/{mention}/, member)
+            .replace(/{user}/, member.user.username)
+            .replace(/{guild}/, member.guild.name)
+            .replace(/{tag}/g, member.user.tag)
+            .replace(/{mCount}/g, member.guild.memberCount);
+    }
+
+};
