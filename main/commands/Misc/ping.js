a47fcfd10fc796b04a2e2ccd7ca0f81dc6437cc6
Phunt05 <37057540+Phunt05@users.noreply.github.com>
2019-04-26 18:51:08 +0530

    bold

--- main/commands/Misc/ping.js
+++ main/commands/Misc/ping.js
@@ -12,7 +12,7 @@ module.exports = class extends Command {
 
 	async run(message) {
 		const msg = await message.send('Pinging');
-		return message.send(`Roundrip took: \`${(msg.editedTimestamp || msg.createdTimestamp) - (message.editedTimestamp || message.createdTimestamp)} ms\` | HeartBeat: \`${Math.round(this.client.ws.ping)} ms\``);
+		return message.send(`**Roundrip took: \`${(msg.editedTimestamp || msg.createdTimestamp) - (message.editedTimestamp || message.createdTimestamp)} ms\` | HeartBeat: \`${Math.round(this.client.ws.ping)} ms\`**`);
 	}
 
 };
